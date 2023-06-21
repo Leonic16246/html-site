467b35010e259e07eced362ccf061f9f29e71edf
Leonic16246 <108039973+Leonic16246@users.noreply.github.com>
2023-06-21 23:39:10 +1200

    Revamped layout and added dark/light themes
    Revamped layout and added dark/light themes using a toggle switch
    added local images for github and linkedin

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+'use strict';
+
+const switcher = document.querySelector('.themeswitch');
+
+switcher.addEventListener('change', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+    
+    const className = document.body.className;
+    if (className == "dark-theme") {
+        document.getElementById("github").src="github-mark-white.png";
+    } else {
+        document.getElementById("github").src="github-mark.png";
+    }
+    console.log('current theme: ' + className);
+
+});
+
+
 const APILINK = "";
 const IMG_PATH = "";
 const SEARCHAPI = "";
